7035165cd383a3668c3c3070025a49ab70edf1b6
Sahil Kumar Singh <sahilkumargreat12@gmail.com>
2026-03-27 21:30:10 +0530

    Enable Spring caching in the backend

--- backend/src/main/java/com/sentri/backend/SentriBackendApplication.java
+++ backend/src/main/java/com/sentri/backend/SentriBackendApplication.java
@@ -2,8 +2,10 @@ package com.sentri.backend;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cache.annotation.EnableCaching;
 
 @SpringBootApplication
+@EnableCaching
 public class SentriBackendApplication {
 
     public static void main(String[] args) {
