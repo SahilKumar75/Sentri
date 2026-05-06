c6034f3a7bb118fb7938d600048341f89dff7887
Sahil Kumar Singh <sahilkumargreat12@gmail.com>
2026-05-06 19:45:05 +0530

    feat(ml): Add constructor for experiment tracker

--- ml-worker/src/sentri_worker/experiment_tracker.py
+++ ml-worker/src/sentri_worker/experiment_tracker.py
@@ -4,4 +4,6 @@ logger = logging.getLogger(__name__)
 
 class ExperimentTracker:
     """Tracks ML experiments."""
-    pass
+    def __init__(self, experiment_name: str):
+        self.experiment_name = experiment_name
+        self.runs = []
