b81ad1df90c8935873faa1b76eac73eaf7f3a7c5
Sahil Kumar Singh <sahilkumargreat12@gmail.com>
2026-05-06 19:59:24 +0530

    feat(ml): Add constructor for DataValidator

--- ml-worker/src/sentri_worker/validator.py
+++ ml-worker/src/sentri_worker/validator.py
@@ -4,4 +4,5 @@ logger = logging.getLogger(__name__)
 
 class DataValidator:
     """Validates ML input data and features."""
-    pass
+    def __init__(self, strict_mode: bool = True):
+        self.strict_mode = strict_mode
