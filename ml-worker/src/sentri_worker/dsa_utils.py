0d92cafc03ff20cba0ce7c0176efdd1a4a43546d
Sahil Kumar Singh <sahilkumargreat12@gmail.com>
2026-05-08 20:10:02 +0530

    feat(dsa): Add merge sort implementation

--- ml-worker/src/sentri_worker/dsa_utils.py
+++ ml-worker/src/sentri_worker/dsa_utils.py
@@ -37,4 +37,29 @@ def binary_search(arr, target):
             if arr[min_idx] > arr[j]:
                 min_idx = j
         arr[i], arr[min_idx] = arr[min_idx], arr[i]
+    return arr\n\ndef merge_sort(arr):
+    """Sort a list using merge sort algorithm."""
+    if len(arr) > 1:
+        mid = len(arr) // 2
+        L = arr[:mid]
+        R = arr[mid:]
+        merge_sort(L)
+        merge_sort(R)
+        i = j = k = 0
+        while i < len(L) and j < len(R):
+            if L[i] < R[j]:
+                arr[k] = L[i]
+                i += 1
+            else:
+                arr[k] = R[j]
+                j += 1
+            k += 1
+        while i < len(L):
+            arr[k] = L[i]
+            i += 1
+            k += 1
+        while j < len(R):
+            arr[k] = R[j]
+            j += 1
+            k += 1
     return arr\n\n
