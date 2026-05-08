a7ecc0e4108f9db507267f840cb110a58ced0a9d
Sahil Kumar Singh <sahilkumargreat12@gmail.com>
2026-05-08 20:10:02 +0530

    feat(dsa): Add selection sort algorithm

--- ml-worker/src/sentri_worker/dsa_utils.py
+++ ml-worker/src/sentri_worker/dsa_utils.py
@@ -29,4 +29,12 @@ def binary_search(arr, target):
             arr[j + 1] = arr[j]
             j -= 1
         arr[j + 1] = key
+    return arr\n\ndef selection_sort(arr):
+    """Sort a list using selection sort algorithm."""
+    for i in range(len(arr)):
+        min_idx = i
+        for j in range(i + 1, len(arr)):
+            if arr[min_idx] > arr[j]:
+                min_idx = j
+        arr[i], arr[min_idx] = arr[min_idx], arr[i]
     return arr\n\n
