c80cd2ed53ee02bf856d022f2d994104b6d9c753
Sahil Kumar Singh <sahilkumargreat12@gmail.com>
2026-05-08 20:10:02 +0530

    feat(dsa): Add insertion sort implementation

--- ml-worker/src/sentri_worker/dsa_utils.py
+++ ml-worker/src/sentri_worker/dsa_utils.py
@@ -20,4 +20,13 @@ def binary_search(arr, target):
                 swapped = True
         if not swapped:
             break
+    return arr\n\ndef insertion_sort(arr):
+    """Sort a list using insertion sort algorithm."""
+    for i in range(1, len(arr)):
+        key = arr[i]
+        j = i - 1
+        while j >= 0 and key < arr[j]:
+            arr[j + 1] = arr[j]
+            j -= 1
+        arr[j + 1] = key
     return arr\n\n
