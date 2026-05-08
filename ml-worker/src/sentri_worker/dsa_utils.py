4474a17397ba49547dd4090896b59a86789a5367
Sahil Kumar Singh <sahilkumargreat12@gmail.com>
2026-05-08 20:10:03 +0530

    feat(dsa): Add Queue data structure

--- ml-worker/src/sentri_worker/dsa_utils.py
+++ ml-worker/src/sentri_worker/dsa_utils.py
@@ -89,4 +89,21 @@ def binary_search(arr, target):
     def peek(self):
         if not self.is_empty():
             return self.items[-1]
-        return None\n\n
+        return None\n\nclass Queue:
+    """A basic queue implementation."""
+    def __init__(self):
+        self.items = []
+        
+    def is_empty(self):
+        return len(self.items) == 0
+        
+    def enqueue(self, item):
+        self.items.insert(0, item)
+        
+    def dequeue(self):
+        if not self.is_empty():
+            return self.items.pop()
+        return None
+        
+    def size(self):
+        return len(self.items)\n\n
